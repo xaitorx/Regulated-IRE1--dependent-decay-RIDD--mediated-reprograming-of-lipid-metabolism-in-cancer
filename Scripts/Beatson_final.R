c036b6ec0a8878339e29ef491abcf10b58a752bd
xaitorx <aitor.almanza@nuigalway.ie>
2022-03-03 18:25:40 -0600

    -

--- Scripts/Beatson_final.R
+++ Scripts/Beatson_final.R
@@ -3,9 +3,7 @@ library("stringr")
 library("ggplot2")
 library("plyr")
 library("reshape2")
-library("svglite")
 library("pheatmap")
-library("ggpubr")
 
 # indicate path to .csv file with average values for each replicate
 todo_treatments <- read.delim(url("https://raw.githubusercontent.com/ASAGlab/Regulated-IRE1-dependent-decay-RIDD-mediated-reprograming-of-lipid-metabolism-in-cancer/main/Data/Beatson_MS.txt"), sep = " ")
@@ -33,74 +31,73 @@ ggplot(PCAi , aes(PC1, PC2, col= as.character(sample_info$replicate))) + geom_po
   ggtitle("PCA. by treatment") +
   theme_bw() 
 
+# probando
 
-### averaged eset for plotting
-final_avg <- as.data.frame(cbind(rowMeans(log_MSTUS_norm[,1:3]), rowMeans(log_MSTUS_norm[,4:6]), rowMeans(log_MSTUS_norm[,7:9]), rowMeans(log_MSTUS_norm[,10:12])))
-row.names(final_avg) <- row.names(log_MSTUS_norm)
-colnames(final_avg) <- c("cotreatment", "DGAT2inh", "DMSO", "MKC8866")
-final_avg <- cbind(todo_treatments[,1:2], final_avg)
+inh_1 <- log_MSTUS_norm$Average.DGAT1 - log_MSTUS_norm$Average.DMSO1
+inh_2 <- log_MSTUS_norm$Average.DGAT2 - log_MSTUS_norm$Average.DMSO2
+inh_3 <- log_MSTUS_norm$Average.DGAT3 - log_MSTUS_norm$Average.DMSO3
 
-# calculate FoldChanges, standard error of mean, error of the ratios
-final_avg$DMSO.sd <- apply(log_MSTUS_norm[,1:3], 1, function(x) sd(as.numeric(x)))
-final_avg$MKC.sd <- apply(log_MSTUS_norm[,4:6], 1, function(x) sd(as.numeric(x)))
-final_avg$DGAT2inh.sd <- apply(log_MSTUS_norm[,7:9], 1, function(x) sd(as.numeric(x)))
-final_avg$cotreat.sd <- apply(log_MSTUS_norm[,10:12], 1, function(x) sd(as.numeric(x)))
+MKC8866_1 <- log_MSTUS_norm$Average.MKC1 - log_MSTUS_norm$Average.DMSO1
+MKC8866_2 <- log_MSTUS_norm$Average.MKC2 - log_MSTUS_norm$Average.DMSO1
+MKC8866_3 <- log_MSTUS_norm$Average.MKC3 - log_MSTUS_norm$Average.DMSO1
 
-final_avg$DvsD2i <- final_avg$DGAT2inh - final_avg$DMSO
-final_avg$Dvscotreat <- final_avg$cotreatment - final_avg$DMSO
-final_avg$DvsM <- final_avg$MKC8866 - final_avg$DMSO
+cot_1 <- log_MSTUS_norm$Average.co.treat.1 - log_MSTUS_norm$Average.DMSO1
+cot_2 <- log_MSTUS_norm$Average.co.treat.2 - log_MSTUS_norm$Average.DMSO1
+cot_3 <- log_MSTUS_norm$Average.co.treat.3 - log_MSTUS_norm$Average.DMSO1
 
-final_avg$DvsD2i.se <- abs(sqrt((final_avg$DGAT2inh.sd/final_avg$DGAT2inh)^2 + (final_avg$DMSO.sd/final_avg$DMSO)^2)*final_avg$DvsD2i)
-final_avg$Dvscotreat.se <- abs(sqrt((final_avg$cotreat.sd/final_avg$cotreatment)^2 + (final_avg$DMSO.sd/final_avg$DMSO)^2)*final_avg$Dvscotreat)
-final_avg$DvsM.se <- abs(sqrt((final_avg$MKC.sd/final_avg$MKC8866)^2 + (final_avg$DMSO.sd/final_avg$DMSO)^2)*final_avg$DvsM)
+final_FCs <- cbind(todo_treatments[,1:2],MKC8866_1, MKC8866_2, MKC8866_3, inh_1, inh_2, inh_3, cot_1, cot_2, cot_3)
+
+# TAGs
+TG <- subset(final_FCs, final_FCs$C..Lipid.Class == "TG")
+
+# duplicated entries
+TG <- ddply(TG,2,numcolwise(mean))
+
+# anotation. Saturation & lenght
+TG$lenght <- sapply(str_extract_all(TG$C..Identification, "[0-9]{2}(?:)"), function(x) sum(as.numeric(x)))
+TG$saturation <- sapply(str_extract_all(TG$C..Identification, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
+numeros_magicos <- cumsum(rev(table(TG$saturation))) + 0.5
+
+TG_avg <- as.data.frame(cbind( ID = TG$C..Identification, 
+                               MKC8866 = rowMeans(TG[,2:4]), 
+                               inh = rowMeans(TG[,5:7]), 
+                               cot = rowMeans(TG[,8:10]),
+                               MKC8866.sd = apply(TG[,2:4], 1, function(x) sd(as.numeric(x))) / sqrt(3), 
+                               inh.sd = apply(TG[,5:7], 1, function(x) sd(as.numeric(x))) / sqrt(3), 
+                               cot.sd = apply(TG[,8:10], 1, function(x) sd(as.numeric(x))) / sqrt(3)))
+
+# format for plotting. Means, SDs, indiv values
+melt1 <- melt(TG, id.vars = 1, measure.vars = 2:10)
+melt_avg <- melt(TG_avg, id.vars = 1, measure.vars = 2:4)
+melt_sd <- melt(TG_avg, id.vars = 1, measure.vars = 5:7)
+melt_sd$variable <- melt_avg$variable
 
 ## formatting, lenght & saturation
-final_avg$lenght <- sapply(str_extract_all(final_avg$C..Identification, "[0-9]{2}(?:)"), function(x) sum(as.numeric(x)))
-final_avg$saturation <- sapply(str_extract_all(final_avg$C..Identification, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
-final_avg$nombre <- paste(final_avg$C..Lipid.Class, final_avg$lenght, ":", final_avg$saturation)
-annotation <- unique(final_avg[,c(1,19)])
+melt1$lenght <- sapply(str_extract_all(melt1$C..Identification, "[0-9]{2}(?:)"), function(x) sum(as.numeric(x)))
+melt1$saturation <- sapply(str_extract_all(melt1$C..Identification, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
+melt1$group <- gsub("\\_.*","",melt1$variable)
 
-# consolidate duplicated entries for same metabolites
-final_avg <- ddply(final_avg,19,numcolwise(mean))
-final_avg <- merge(annotation, final_avg, by.x = 2, by.y = 1)
-row.names(final_avg) <- final_avg$nombre
+melt1 <- melt1[order(melt1$lenght, decreasing = TRUE),]
+melt1 <- melt1[order(melt1$saturation, decreasing = TRUE),]
+melt1$C..Identification <- factor(melt1$C..Identification, levels = unique(melt1$C..Identification))
+melt1$variable <- factor(melt1$variable, levels = c("cot", "inh", "MKC8866"))
 
-### TAKE CLOSER LOOK AT TAGS
-### HEATMAP
-TAGs <- subset(final_avg, final_avg$C..Lipid.Class == "TG")
-TAGs <- TAGs[order(TAGs$lenght),]
-TAGs <- TAGs[order(TAGs$saturation),]
+melt_avg$ID <- factor(melt_avg$ID, levels = levels(melt1$C..Identification))
+melt_avg$variable <- factor(melt_avg$variable, levels = levels(melt1$variable))
 
-my_palette <- colorRampPalette(c("navy","white","red"))(n = 20) # or whatever colors and number
-pheatmap(na.omit(TAGs[,c(5,6,4,3)]), 
-         cluster_cols=FALSE, 
-         cluster_rows = FALSE,
-         color = my_palette, 
-         border_color = NA, 
-         fontsize_col = 14, 
-         scale = "row",
-         show_rownames = TRUE)
-
-### Plot changes for all TAG species
-numeros_magicos <- cumsum(rev(table(TAGs$saturation))) + 0.5
-melt_uno <- melt(TAGs, id.vars = 1, measure.vars = 11:13)
-melt_dos <- melt(TAGs, id.vars = 1, measure.vars = 14:16)
-melt_todo <- cbind(melt_uno, melt_dos[,3])
-melt_todo$nombre <- factor(melt_todo$nombre, levels = rev(unique(melt_todo$nombre)))
-melt_todo$variable <- gsub("DvsM", "MKC8866", melt_todo$variable)
-melt_todo$variable <- gsub("DvsD2i", "PF-06427878", melt_todo$variable)
-melt_todo$variable <- gsub("Dvscotreat", "Cotreatment", melt_todo$variable)
-melt_todo$variable <- factor(melt_todo$variable, levels = c("Cotreatment","PF-06427878","MKC8866"))
+melt_sd$ID <- factor(melt_sd$ID, levels = levels(melt1$C..Identification))
+melt_sd$variable <- factor(melt_sd$variable, levels = levels(melt1$variable))
 
 ggplot() +
-  geom_bar(data=melt_todo, aes(fill=melt_todo$variable, y=melt_todo$value,x=melt_todo$nombre, ymin=melt_todo$value-melt_todo$`melt_dos[, 3]`,ymax=melt_todo$value+melt_todo$`melt_dos[, 3]`), position="dodge", stat="identity", color="black") +
-  geom_errorbar(data=melt_todo, aes(fill=melt_todo$variable, y=melt_todo$value,x=melt_todo$nombre,ymin=melt_todo$value-melt_todo$`melt_dos[, 3]`,ymax=melt_todo$value+melt_todo$`melt_dos[, 3]`), position="dodge",stat="identity") +
-  labs(x = "", y = "log2 FC (MKC8866 vs control)", element_text(face = "bold", angle = 0)) +
+  geom_bar(data=melt_avg, aes(fill= `variable`, y= as.numeric(melt_avg$value), x= `ID`),position="dodge", stat="identity", color="black") +
+  geom_errorbar(data=melt_sd, aes(fill = `variable`, y=as.numeric(melt_avg$value) ,x=`ID`, ymin=as.numeric(melt_avg$value)-as.numeric(melt_sd$value),ymax=as.numeric(melt_avg$value)+as.numeric(melt_sd$value)), position=position_jitterdodge(jitter.width=0.1, dodge.width=0.9),stat="identity") +
+  geom_point(data=melt1 ,pch=21, colour="black", aes(fill = `variable`,  group= `group`, y=melt1$value,x=melt1$C..Identification), position=position_jitterdodge(jitter.width=0.1, dodge.width=0.9)) +
+  labs(x = "number of double bonds", y = "log2 FC (MKC8866 vs control)", element_text(face = "bold", angle = 0)) +
+  geom_hline(yintercept = 0, size = 1) +
   coord_flip() +
   geom_vline(xintercept = numeros_magicos[-length(numeros_magicos)] ,linetype = 2, size = 1, color = "black") 
-  
 
-### saturation & elongation indexes TGs
+
 
 
 
--- Scripts/Metasyx_final.R
+++ Scripts/Metasyx_final.R
@@ -12,12 +12,13 @@ library("plyr")
 # data: samples in columns, features in rows
 todo_treatments <- read.csv(url("https://raw.githubusercontent.com/ASAGlab/Regulated-IRE1-dependent-decay-RIDD-mediated-reprograming-of-lipid-metabolism-in-cancer/main/Data/Metasyx_MD.csv"))
 Sample_Info <- read.csv(url("https://raw.githubusercontent.com/ASAGlab/Regulated-IRE1-dependent-decay-RIDD-mediated-reprograming-of-lipid-metabolism-in-cancer/main/Data/Metasyx_sample_info.csv"))
+anotacion <- todo_treatments[,c(1,2,10)]
 
 # MSTUS NORMALIZATION (normalized to sum of peak areas)
 MSTUS_norm <- sweep(todo_treatments[,12:ncol(todo_treatments)] ,2,colSums(todo_treatments[,12:ncol(todo_treatments)])/100000000,`/`)
 row.names(MSTUS_norm) <- todo_treatments$Peak_ID
 
-# LOG2 scale (plotting)
+# LOG2 scale 
 log_MSTUS_norm <- log2(MSTUS_norm)
 
 ### PCA
@@ -31,7 +32,7 @@ ggplot(PCAi , aes(PC1, PC2, col= Sample_Info$Group)) + geom_point(aes(size=3)) +
   theme_bw() 
 
 #PCA by repeat
-ggplot(PCAi , aes(PC1, PC2, col= as.character(Sample_Info$replicate))) + geom_point(aes(size=3)) + geom_text(aes(label = row.names(PCAi)), nudge_y = 1) + 
+ggplot(PCAi , aes(PC1, PC2, col= as.factor(Sample_Info$replicate))) + geom_point(aes(size=3)) + geom_text(aes(label = row.names(PCAi)), nudge_y = 1) + 
   labs (y = paste("PC2", round(eigs[2] / sum(eigs), digits = 2)), x = paste("PC1", round(eigs[1] / sum(eigs), digits = 2))) + # substitute with % variances
   ggtitle("PCA. by treatment") +
   theme_bw() 
@@ -42,7 +43,7 @@ mod1 <- model.matrix(~0 + Group, Sample_Info)
 mod0 <- model.matrix(~1, Sample_Info)
 svobj <- svaseq(as.matrix(MSTUS_norm), mod1, mod0) 
 
-# "Clean" dataset
+# "Clean" dataset. regress identified sva from data
 cleanY = function(y, mod, svs) {
   X = cbind(mod, svs)
   Hat = solve(t(X) %*% X) %*% t(X)
@@ -72,48 +73,45 @@ ggplot(PCAi , aes(PC1, PC2, col= Sample_Info$Group)) + geom_point(aes(size=3)) +
   ggtitle("PCA. by treatment") +
   theme_bw() 
 
-### averaged eset for plotting
-final_avg <- as.data.frame(cbind(rowMeans(cleaned_count[,1:5]), rowMeans(cleaned_count[,6:10]), rowMeans(cleaned_count[,11:15]), rowMeans(cleaned_count[,16:20]), rowMeans(cleaned_count[,21:25]), rowMeans(cleaned_count[,26:30])))
-colnames(final_avg) <- c( "DMSO24", "MKC24", "DMSO48", "MKC48", "DMSO72", "MKC72")
-final_avg <- cbind(todo_treatments[,c(1,2,10)], final_avg)
+# fold changes for each replicate individually
+ind <- which(Sample_Info$Treatment == "DMSO")
 
-# standard deviation (SD)
-final_avg$DMSO24.sd <- apply(cleaned_count[,1:5], 1, function(x) sd(as.numeric(x)))
-final_avg$MKC24.sd <- apply(cleaned_count[,6:10], 1, function(x) sd(as.numeric(x)))
-final_avg$DMSO48.sd <- apply(cleaned_count[,11:15], 1, function(x) sd(as.numeric(x)))
-final_avg$MKC48.sd <- apply(cleaned_count[,16:20], 1, function(x) sd(as.numeric(x)))
-final_avg$DMSO72.sd <- apply(cleaned_count[,21:25], 1, function(x) sd(as.numeric(x)))
-final_avg$MKC72.sd <- apply(cleaned_count[,26:30], 1, function(x) sd(as.numeric(x)))
+for (i in list(ind)){
+  ooo <- cleaned_count[,i+5] - cleaned_count[,i]
+}
 
-# MKC/DMSO log2 fold changes
-final_avg$DvsM24h <- final_avg$MKC24 - final_avg$DMSO24
-final_avg$DvsM48h <- final_avg$MKC48 - final_avg$DMSO48
-final_avg$DvsM72h <- final_avg$MKC72 - final_avg$DMSO72
+FCs <- merge(anotacion, ooo, by.x = 1, by.y = 0)
 
-# add lenght and saturation columns
-final_avg$lenght <- sapply(str_extract_all(final_avg$Compound_Name, "[0-9]+(?:)"), function(x) sum(as.numeric(x)))
-final_avg$saturation <- sapply(str_extract_all(final_avg$Compound_Name, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
-final_avg$lenght <- final_avg$lenght - final_avg$saturation #bad regex 
+FCs_avg <- as.data.frame(cbind("24h" = rowMeans(FCs[,4:8]), 
+                               "48h" = rowMeans(FCs[,9:13]), 
+                               "72h" = rowMeans(FCs[,14:18]),
+                               "24h.sd" = apply(FCs[,4:8], 1, function(x) sd(as.numeric(x))) / sqrt(5), 
+                               "48h.sd" = apply(FCs[,9:13], 1, function(x) sd(as.numeric(x))) / sqrt(5), 
+                               "72h.sd" = apply(FCs[,14:18], 1, function(x) sd(as.numeric(x))) / sqrt(5)))
 
-# solo lipids
-solo_lipids <- subset(final_avg, final_avg$Lipid_Class != "Amino Acids") #also removes NA
+FCs_avg <- cbind(FCs[,1:3], FCs_avg)
 
+##lenght & saturation
+FCs$lenght <- sapply(str_extract_all(FCs$Compound_Name, "[0-9]{2}(?=:)"), function(x) sum(as.numeric(x)))
+FCs$saturation <- sapply(str_extract_all(FCs$Compound_Name, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
 
+FCs_avg$lenght <- sapply(str_extract_all(FCs$Compound_Name, "[0-9]{2}(?=:)"), function(x) sum(as.numeric(x)))
+FCs_avg$saturation <- sapply(str_extract_all(FCs$Compound_Name, "(?<=:)[0-9]+"), function(x) sum(as.numeric(x)))
 
 # HEATMAP OF FOLD CHANGES FOR ALL LIPIDS THROUGH 3 TIME POINTS
-annotation_row <- as.data.frame(solo_lipids$Lipid_Class)
-row.names(annotation_row) <- solo_lipids$Peak_ID
-row.names(solo_lipids) <- solo_lipids$Peak_ID
+annotation_row <- as.data.frame(FCs_avg$Lipid_Class)
+row.names(annotation_row) <- FCs_avg$Peak_ID
+row.names(FCs_avg) <- FCs_avg$Peak_ID
 
-newCols <- rev(primary.colors(length(unique(annotation_row$`solo_lipids$Lipid_Class`)))) #reverse just cause I didnt like colors
-names(newCols) <- unique(annotation_row$`solo_lipids$Lipid_Class`)
-newCols <- list(`solo_lipids$Lipid_Class` = newCols)
+newCols <- rev(primary.colors(length(unique(FCs_avg$Lipid_Class)))) #reverse just cause I didnt like colors
+names(newCols) <- unique(FCs_avg$Lipid_Class)
+newCols <- list("FCs_avg$Lipid_Class" = newCols)
 
 my_palette <- colorRampPalette(c("navy","white","red"))(n = 20) # or whatever colors and number
 myBreaks <- c(seq(-1, 0, length.out=ceiling(length(my_palette)/2) + 1), 
               seq(1/length(my_palette), 1, length.out=floor(length(my_palette)/2))) # from -1 to 1 log2FC 
 
-pheatmap(na.omit(solo_lipids[,16:18]), 
+pheatmap(na.omit(FCs_avg[,4:6]), 
          cluster_cols=FALSE, 
          color = my_palette, 
          border_color = NA, 
@@ -124,32 +122,34 @@ pheatmap(na.omit(solo_lipids[,16:18]),
          breaks = myBreaks)
 
 
-# lipid group through time
+# explore each lipid group changes through time
 timeplot = function(lipido, color) {
-  ooo <- subset(solo_lipids, solo_lipids$Lipid_Class == lipido)
-  melt_ooo <- melt(ooo, id.vars = 3, measure.vars = 16:18)
-  melt_ooo$variable <- gsub("DvsM", "", melt_ooo$variable)
-  numero <- round(max(melt_ooo$value + 0.1), digits = 1)
-  numero2 <- round(min(melt_ooo$value - 0.1), digits = 1)
-  ggplot() +  geom_point(data=melt_ooo,pch=21, size=3, colour="black", aes(fill= `Lipid_Class`, group= `variable`, y= `value`,x= `variable`), position=position_jitterdodge(jitter.width=0.4, dodge.width=0.9)) +
+  ooo <- subset(FCs_avg, FCs_avg$Lipid_Class == lipido)
+  melt_ooo <- melt(ooo, id.vars = 1, measure.vars = 4:6)
+  p <- ggplot(melt_ooo, aes(melt_ooo$variable, melt_ooo$value))
+  p + geom_boxplot(outlier.shape = NA, size =2, alpha = 0.5) + 
+    geom_point(data=melt_ooo,pch=21, size=3, colour="black", aes(fill= lipido, group= `variable`, y= `value`,x= `variable`), position=position_jitterdodge(jitter.width=0.4, dodge.width=0.9)) +
     theme_classic() +
     theme(text = element_text(size=20), axis.title.y = element_text(size=16), legend.position = "none") +
     ggtitle(lipido) +
     scale_fill_manual(values = color) +
     geom_hline(yintercept = 0 ,linetype = 2, size = 1, color = "black") +
-    ylim(numero2, numero) +
+    ylim(-1, 1.2) +
     labs(x="", y = "Log2 FC (MKC8866/DMSO)", element_text(face = "bold", angle = 0,)) + 
-    annotate('text', x = c("24h", "48h", "72h"), y = numero ,label = c(paste("P-value==", t.test(ooo$DvsM24, mu = 0, alternative = "two.sided")$p.value),
-                                                                       paste("P-value==", t.test(ooo$DvsM48, mu = 0, alternative = "two.sided")$p.value),
-                                                                       paste("P-value==", t.test(ooo$DvsM72, mu = 0, alternative = "two.sided")$p.value)) ,parse = TRUE, size=3)
+    annotate('text', x = c("24h", "48h", "72h"), y = 1.1 ,label = c(paste("P-value==", t.test(ooo$`24h`, mu = 0, alternative = "two.sided")$p.value),
+                                                                    paste("P-value==", t.test(ooo$`48h`, mu = 0, alternative = "two.sided")$p.value),
+                                                                    paste("P-value==", t.test(ooo$`72h`, mu = 0, alternative = "two.sided")$p.value)) ,parse = TRUE, size=3)
 }
 
+# check what lipid groups there are
+unique(FCs_avg$Lipid_Class)
+# example, lets plot "Triacylglyceride"
+timeplot("Triacylglyceride", "red")
 
-timeplot("Phosphatidylglycerol", "green")
 
 # plot all lipid groups. using same colors as in heatmap
-for (i in unique(solo_lipids$Lipid_Class)) {
-  g <- timeplot(i, newCols$`solo_lipids$Lipid_Class`[i])
+for (i in unique(FCs_avg$Lipid_Class)) {
+  g <- timeplot(i, newCols$`FCs_avg$Lipid_Class`[i])
   print(g) 
   #ggsave( paste( "time", i, ".pdf"), plot=last_plot(), width=8, height=5)
 }
@@ -157,38 +157,29 @@ for (i in unique(solo_lipids$Lipid_Class)) {
 #or plot them all together to evaluate in 1 look
 library(gridExtra)
 p <- list()
-for (i in unique(solo_lipids$Lipid_Class)) {
-  p[[i]] <- timeplot(i, newCols$`solo_lipids$Lipid_Class`[i])
+for (i in unique(FCs_avg$Lipid_Class)) {
+  p[[i]] <- timeplot(i, newCols$`FCs_avg$Lipid_Class`[i])
 }
 do.call(grid.arrange,p)
 
-### saturation & elongation indexes TGs
+### saturation & elongation indexes 
 # plot by saturation
-# divided in 0, 1,2,3+ saturated bonds
+# divide in groups: 0,1,2,3+ saturated bonds
 satuplot = function(lipido, color) {
-  ooo <- subset(final_avg, final_avg$Lipid_Class == lipido)
-  uuu <- unique(ooo$saturation)
+  ooo <- subset(FCs_avg, FCs_avg$Lipid_Class == lipido)
   ooo$saturange <- NA
   ooo$saturange[ooo$saturation == 0] <-  "0"
   ooo$saturange[ooo$saturation == 1] <-  "1"
   ooo$saturange[ooo$saturation == 2] <-  "2"
   ooo$saturange[ooo$saturation >= 3] <-  "3+"
-  average_tri <- ddply(ooo,21,numcolwise(mean))
-  average_tri$se24 <- c(sd(ooo$DvsM24[ooo$saturange == "0"]), sd(ooo$DvsM24[ooo$saturange == "1"]), sd(ooo$DvsM24[ooo$saturange == "2"]), sd(ooo$DvsM24[ooo$saturange == "3+"]))
-  average_tri$se48 <- c(sd(ooo$DvsM24[ooo$saturange == "0"]), sd(ooo$DvsM48[ooo$saturange == "1"]), sd(ooo$DvsM48[ooo$saturange == "2"]), sd(ooo$DvsM48[ooo$saturange == "3+"]))
-  average_tri$se72 <- c(sd(ooo$DvsM24[ooo$saturange == "0"]), sd(ooo$DvsM72[ooo$saturange == "1"]), sd(ooo$DvsM72[ooo$saturange == "2"]), sd(ooo$DvsM72[ooo$saturange == "3+"]))
-  melt2 <- melt(average_tri, id.vars = 1, measure.vars = 14:16)
-  melt3 <- melt(average_tri, id.vars = 1, measure.vars = 19:21)
+  average_tri <- ddply(ooo,12,numcolwise(mean))
+  average_tri$se24 <- c(sd(ooo$`24h`[ooo$saturange == "0"]), sd(ooo$`24h`[ooo$saturange == "1"]), sd(ooo$`24h`[ooo$saturange == "2"]), sd(ooo$`24h`[ooo$saturange == "3+"]))
+  average_tri$se48 <- c(sd(ooo$`48h`[ooo$saturange == "0"]), sd(ooo$`48h`[ooo$saturange == "1"]), sd(ooo$`48h`[ooo$saturange == "2"]), sd(ooo$`48h`[ooo$saturange == "3+"]))
+  average_tri$se72 <- c(sd(ooo$`72h`[ooo$saturange == "0"]), sd(ooo$`72h`[ooo$saturange == "1"]), sd(ooo$`72h`[ooo$saturange == "2"]), sd(ooo$`72h`[ooo$saturange == "3+"]))
+  melt2 <- melt(average_tri, id.vars = 1, measure.vars = 2:4)
+  melt3 <- melt(average_tri, id.vars = 1, measure.vars = 10:12)
   melt_todo <- cbind(melt2, melt3[,3])
-  melt_todo$variable <- gsub("DvsM24", "24h", melt_todo$variable)
-  melt_todo$variable <- gsub("DvsM48", "48h", melt_todo$variable)
-  melt_todo$variable <- gsub("DvsM72", "72h", melt_todo$variable)
-  melt_dots <- melt(ooo, id.vars = 21, measure.vars = 16:18)
-  melt_dots$variable <- gsub("DvsM24", "24h", melt_dots$variable)
-  melt_dots$variable <- gsub("DvsM48", "48h", melt_dots$variable)
-  melt_dots$variable <- gsub("DvsM72", "72h", melt_dots$variable)
-  numero <- round(max(melt_dots$value + 0.1), digits = 1)
-  numero2 <- round(min(melt_dots$value - 0.1), digits = 1)
+  melt_dots <- melt(ooo, id.vars = 12, measure.vars = 4:6)
   ggplot() +
     geom_bar(data=melt_todo, aes(fill=melt_todo$variable, y=melt_todo$value,x=melt_todo$saturange,ymin=melt_todo$value-melt_todo$`melt3[, 3]`,ymax=melt_todo$value+melt_todo$`melt3[, 3]`), position="dodge", stat="identity", color="black") +
     geom_errorbar(data=melt_todo, aes(fill=melt_todo$variable, y=melt_todo$value,x=melt_todo$saturange,ymin=melt_todo$value-melt_todo$`melt3[, 3]`,ymax=melt_todo$value+melt_todo$`melt3[, 3]`), position="dodge",stat="identity") +
@@ -199,34 +190,27 @@ satuplot = function(lipido, color) {
     geom_hline(yintercept = 0, linetype="dashed", size = 2)
 }
 
+satuplot("Triacylglyceride"  , "green")
+
 # elongation
 # plot by total chain lenght
 # divided into ranges matching the 4 quartiles
-
 lenghtplot = function(lipido, color) {
-  ooo <- subset(final_avg, final_avg$Lipid_Class == lipido)
+  ooo <- subset(FCs_avg, FCs_avg$Lipid_Class == lipido)
   uuu <- summary(ooo$lenght)
   ooo$saturange <- NA
   ooo$saturange[ooo$lenght <= round(uuu[[2]])] <- paste( "<" , round(uuu[[2]]))
   ooo$saturange[ooo$lenght > round(uuu[[2]])] <- paste(round(uuu[[2]]),"-", round(uuu[[3]]))
   ooo$saturange[ooo$lenght > round(uuu[[3]])] <- paste(round(uuu[[3]]),"-", round(uuu[[5]]))
   ooo$saturange[ooo$lenght >= round(uuu[[5]])] <- paste( ">" , round(uuu[[5]]))
-  average_tri <- ddply(ooo,21,numcolwise(mean))
-  average_tri$se24 <- c(sd(ooo$DvsM24[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$DvsM24[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$DvsM24[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$DvsM24[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
-  average_tri$se48 <- c(sd(ooo$DvsM24[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$DvsM48[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$DvsM48[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$DvsM48[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
-  average_tri$se72 <- c(sd(ooo$DvsM24[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$DvsM72[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$DvsM72[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$DvsM72[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
-  melt2 <- melt(average_tri, id.vars = 1, measure.vars = 14:16)
-  melt3 <- melt(average_tri, id.vars = 1, measure.vars = 19:21)
+  average_tri <- ddply(ooo,12,numcolwise(mean))
+  average_tri$se24 <- c(sd(ooo$`24h`[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$`24h`[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$`24h`[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$`24h`[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
+  average_tri$se48 <- c(sd(ooo$`48h`[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$`48h`[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$`48h`[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$`48h`[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
+  average_tri$se72 <- c(sd(ooo$`72h`[ooo$saturange == paste( "<" , round(uuu[[2]]))]), sd(ooo$`72h`[ooo$saturange == paste(round(uuu[[2]]),"-", round(uuu[[3]]))]), sd(ooo$`72h`[ooo$saturange == paste(round(uuu[[3]]),"-", round(uuu[[5]]))]), sd(ooo$`72h`[ooo$saturange == paste( ">" , round(uuu[[5]]))]))
+  melt2 <- melt(average_tri, id.vars = 1, measure.vars = 2:4)
+  melt3 <- melt(average_tri, id.vars = 1, measure.vars = 10:12)
   melt_todo <- cbind(melt2, melt3[,3])
-  melt_todo$variable <- gsub("DvsM24", "24h", melt_todo$variable)
-  melt_todo$variable <- gsub("DvsM48", "48h", melt_todo$variable)
-  melt_todo$variable <- gsub("DvsM72", "72h", melt_todo$variable)
-  melt_dots <- melt(ooo, id.vars = 21, measure.vars = 16:18)
-  melt_dots$variable <- gsub("DvsM24", "24h", melt_dots$variable)
-  melt_dots$variable <- gsub("DvsM48", "48h", melt_dots$variable)
-  melt_dots$variable <- gsub("DvsM72", "72h", melt_dots$variable)
-  numero <- round(max(melt_dots$value + 0.1), digits = 1)
-  numero2 <- round(min(melt_dots$value - 0.1), digits = 1)
+  melt_dots <- melt(ooo, id.vars = 12, measure.vars = 4:6)
   melt_todo$saturange <- factor(melt_todo$saturange, levels = c(paste( "<" , round(uuu[[2]])), paste(round(uuu[[2]]),"-", round(uuu[[3]])), paste(round(uuu[[3]]),"-", round(uuu[[5]])), paste( ">" , round(uuu[[5]]))))
   ggplot() +
     geom_bar(data=melt_todo, aes(fill=melt_todo$variable, y=melt_todo$value,x=melt_todo$saturange,ymin=melt_todo$value-melt_todo$`melt3[, 3]`,ymax=melt_todo$value+melt_todo$`melt3[, 3]`), position="dodge", stat="identity", color="black") +
@@ -238,11 +222,45 @@ lenghtplot = function(lipido, color) {
     geom_hline(yintercept = 0, linetype="dashed", size = 2)
 }
 
+# example
+unique(FCs_avg$Lipid_Class)
 
-# For TAGs
-unique(final_avg$Lipid_Class)
+satuplot("Sphyngomyelin"  , "blue")
+lenghtplot("Triacylglyceride"  , "magenta")
 
-satuplot("Fatty Acid"  , "limegreen"  )
-lenghtplot("Fatty Acid"  , "limegreen"  )
+# extra bomboclap
+# consolidate duplicated entries (mean)
+FCs_o <- ddply(FCs ,2,numcolwise(mean))
+FCs_avg_o <- ddply(FCs_avg ,2,numcolwise(mean))
 
+#anotar 
+anot <- unique(anotacion[,2:3])
 
+FCs_o <- merge(anot, FCs_o, by.x = 1, by.y = 1)
+FCs_avg_o <- merge(anot, FCs_avg_o, by.x = 1, by.y = 1)
+
+# plot
+probandou = function(lipido) {
+  ooo <- subset(FCs_o, FCs_o$Lipid_Class == lipido )
+  uuu <- subset(FCs_avg_o, FCs_avg_o$Lipid_Class == lipido)
+  ooo <- ooo[order(ooo$lenght, decreasing = TRUE),]
+  ooo <- ooo[order(ooo$saturation, decreasing = TRUE),]
+  niveles <- unique(ooo$Compound_Name)
+  melt_ooo <- melt(ooo, id.vars = 1, measure.vars = 13:17)
+  melt_ooo$Compound_Name <- factor(melt_ooo$Compound_Name, levels = niveles)
+  uuu$Compound_Name <- factor(uuu$Compound_Name, levels = niveles)
+  numeros_magicos <- cumsum(rev(table(ooo$saturation))) + 0.5
+  numero1 <- round(max(melt_ooo$value + 0.1), digits = 1)
+  numero2 <- round(min(melt_ooo$value - 0.1), digits = 1)
+  ggplot() +
+    geom_bar(data=uuu, aes( y= as.numeric(uuu$`72h`), x= uuu$Compound_Name),position="dodge", stat="identity", color="black") +
+    geom_errorbar(data=uuu, aes(y=as.numeric(uuu$`72h`) ,x= uuu$Compound_Name, ymin=as.numeric(uuu$`72h`)-as.numeric(uuu$`72h.sd`),ymax=as.numeric(uuu$`72h`)+as.numeric(uuu$`72h.sd`), position= "dodge",stat="identity")) +
+    geom_point(data=melt_ooo ,pch=21, fill ="black", colour="black", aes( y=as.numeric(melt_ooo$value), x=melt_ooo$Compound_Name)) +
+    labs(x = "number of double bonds", y = "log2 FC (MKC8866 vs control)", element_text(face = "bold", angle = 0)) +
+    geom_hline(yintercept = 0, size = 1) +
+    ylim(numero2, numero1) +
+    coord_flip() +
+    geom_vline(xintercept = numeros_magicos[-length(numeros_magicos)] ,linetype = 2, size = 1, color = "black") 
+}
+
+probandou("Lyso Phosphatidylcholine")  
